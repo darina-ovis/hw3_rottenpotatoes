6a109557889ca284bfc2fa56f3248b45717d46fc
Darina Nikolaeva <kitten.g.a.v@gmail.com>
2012-03-26 03:29:40 -0700

    hw3-part2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,17 @@
+Then /^I should see all of the movies$/ do
+  rows = page.all('table#movies>tbody>tr').count
+  rows.should == Movie.all.count
+end
+
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(:title => movie[:title], :rating => movie[:rating], :release_date=>movie[:release_date])
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  assert false, "Unimplmemented"
+  # assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -22,6 +28,14 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+    rating_list.split(', ').each do |rating|
+      if uncheck.nil?
+        step %Q{I check "ratings_#{rating}"}
+      else
+        step %Q{I uncheck "ratings_#{rating}"}
+      end
+    end
+    
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
